785be0f3f1240c691fde35806a987a11d70cc4f7
Prashant <prashant.bucha@comprotechnologies.com>
2018-05-02 15:46:01 +0530

    Updating Code to run code

--- NodeMongo/index.js
+++ NodeMongo/index.js
@@ -1,53 +1,36 @@
 const mongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+//const assert = require('assert');
 const dboper = require('./operations');
 
-const url = "mongodb://localhost:27017/conFusion";
+const url = "mongodb://127.0.0.1:27017/conFusion";
 
-mongoClient.connect(url)
-.then((err,db)=>{
-    assert.equal(err,null);
+mongoClient.connect(url, function(err,db){
+    //assert.equal(err,null);
     console.log("Connected to server correctly");
     const collection = db.collection("dishes");
-    
     dboper.insertDoc(db, { name: "Vadonut", description: "Test"}, "dishes")
     .then((result)=>{
-        console.log("\nInsert Document: " +result.ops);
+        console.log("\nInsert Document: " + JSON.stringify(result.ops));
         return dboper.findDoc(db, "dishes");
     })
-    .catch((err) => {
-        console.log(err);
-    })
     .then((docs) => {
-        assert.equal(err,null);
-        console.log("\nFound: " + docs);
+        //assert.equal(err,null);
+        console.log("\nFound: " + JSON.stringify(docs));
         return dboper.updateDoc(db, { name: "Vadonut" },{ description: "Updated Test" }, "dishes")
     })
-    .catch((err) => {
-        console.log(err);
-    })
     .then((result) => {
-        console.log("Updated Document:\n", result.result);                    
+        console.log("Updated Document:\n", JSON.stringify(result.result));
         return dboper.findDoc(db, "dishes")
     })
-    .catch((err) => {
-        console.log(err);
-    })
     .then((docs) => {
-        assert.equal(err,null);
-        console.log("\nFound: " + docs);
+        //assert.equal(err,null);
+        console.log("\nFound: " + JSON.stringify(docs));
         return db.dropCollection("dishes")
     })
-    .catch((err) => {
-        console.log(err);
-    })
     .then((result) => {
-        console.log("Dropped Collection: ", result);
+        console.log("Dropped Collection: ", JSON.stringify(result));
         db.close();
     })
-    .catch((err) => {
-        console.log(err);
-    })
-.catch((err) => {console.log(err);})
+    .catch((err) => {console.log(err)});
 });
 
